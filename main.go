ecb4b87381da57ec977b0895ee36cc6b307834cf
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-01-19 09:06:34 +0530

    feat: add per user stats

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 
+	"strconv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -73,21 +73,45 @@ func main() {
 	fmt.Println("Server is running on http://localhost:8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
-
-// Handler function to count occurrences of each ID
+// Handler function to count occurrences of all IDs or a specific ID
 func countIDOccurrencesHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")  // You may change this to a more specific origin if needed
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 	collection := client.Database("Telegram").Collection("CrocEn")
 
-	// Aggregation pipeline to count occurrences of each ID
-	pipeline := mongo.Pipeline{
-		{{"$group", bson.D{
-			{Key: "_id", Value: "$ID"},                                    // Group by the "ID" field
-			{Key: "count", Value: bson.D{{Key: "$sum", Value: 1}}},        // Count occurrences
-			{Key: "Name", Value: bson.D{{Key: "$first", Value: "$Name"}}}, // Get the first "Name" encountered for the grouped ID
-		}}},
-		{{"$sort", bson.D{{Key: "count", Value: -1}}}}, // Sort by count in descending order
+	// Extract the optional ID query parameter
+	id := r.URL.Query().Get("id")
+
+	// Build the aggregation pipeline
+	var pipeline mongo.Pipeline
+	if id != "" {
+		// Convert the ID to an integer (MongoDB might store it as an integer)
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			http.Error(w, "Invalid ID format. ID must be an integer.", http.StatusBadRequest)
+			return
+		}
+
+		// If an ID is provided, filter by the specific ID
+		pipeline = mongo.Pipeline{
+			{{"$match", bson.D{{Key: "ID", Value: idInt}}}}, // Match documents with the specified ID
+			{{"$group", bson.D{
+				{Key: "_id", Value: "$ID"},                             // Group by the "ID" field
+				{Key: "count", Value: bson.D{{Key: "$sum", Value: 1}}}, // Count occurrences
+				{Key: "Name", Value: bson.D{{Key: "$first", Value: "$Name"}}}, // Get the first "Name" encountered for the grouped ID
+			}}},
+		}
+	} else {
+		// If no ID is provided, aggregate for all IDs
+		pipeline = mongo.Pipeline{
+			{{"$group", bson.D{
+				{Key: "_id", Value: "$ID"},                             // Group by the "ID" field
+				{Key: "count", Value: bson.D{{Key: "$sum", Value: 1}}}, // Count occurrences
+				{Key: "Name", Value: bson.D{{Key: "$first", Value: "$Name"}}}, // Get the first "Name" encountered for the grouped ID
+			}}},
+			{{"$sort", bson.D{{Key: "count", Value: -1}}}}, // Sort by count in descending order
+		}
 	}
 
 	// Execute the aggregation query
@@ -113,3 +137,4 @@ func countIDOccurrencesHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println("Error encoding JSON:", err)
 	}
 }
+
